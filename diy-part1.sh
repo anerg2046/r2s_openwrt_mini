e1f5d8d1f56462487edeb03e18edda1bc6e9223c
Coeus <r.anerg@gmail.com>
2020-12-20 22:19:56 +0800

    patch

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,6 +2,3 @@
 # Add a feed source
 echo -e '\nsrc-git lienol https://github.com/xiaorouji/openwrt-passwall' >> feeds.conf.default
 
-# Swap Lan Wan
-cd openwrt
-git apply ../patch/r2s_swap_lan_wan.diff
